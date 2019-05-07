aeff91c7feffee4f29150f88b1e9d7d12e306dc0
Nizynski <nizynski@egb070000003.lancs.local>
2019-05-07 13:40:13 +0100

    I have created 4 balls to start to represent Graph 1, To make on 1 i added ball to the driver but to make 4 i have made 2 loops one to make an array of balls and one to add the balls to the arean. i am likely to change this to A graph 1 class.

--- Driver.java
+++ Driver.java
@@ -1,15 +1,33 @@
-public class Driver
-{
-    
+public class Driver{
 
-    public static void main(String[]args)
-    {
-        GameArena arena = new GameArena(500,500);
-        Node node =new Node(250,250,20,"BLUE");
-        
-        arena.addBall(node);
+
+public static void main(String[]args){
+
+     GameArena arena = new GameArena(500,500);
+    Ball nodeArray[] = new Ball[4];
+
+    for(int i = 0;i< 4;i++){
+        if (i==0){
+            nodeArray[i]= new Ball(150,250,20,"BLUE");
+        }
+        else if(i==1){
+            nodeArray[i] = new Ball(250,150,20,"GREEN");
+        }
+        else if(i==2){
+            nodeArray[i] = new Ball(250,250,20,"RED");
+        }
+        else{
+            nodeArray[i]= new Ball(150,150,20,"YELLOW");
+        }
+    }
+       
+        //Ball node =new Node(250,250,20,"BLUE");
+        for( int i=0;i<4;i++){
+            arena.addBall(nodeArray[i]);
+           
+        }
+
         arena.update();
         
-        //arena.node(arena);
     }
 }
