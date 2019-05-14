4c45c79da8d56deebf1d3cc34134b5a2c9b5d195
Nizynski <nizynski@egb070000049.lancs.local>
2019-05-14 14:12:56 +0100

    This is the content from driver which will be used to make the first graph and called in the main in driver

--- Graph.java
+++ Graph.java
@@ -1,41 +1,58 @@
 //File:Graph.java
 
-public class Graph{
+/*
+*
+* @author katie nizynski
+*/
 
-private int matrix[][];
-private int verticies;
-
-
-public Graph(int verticies)
+public class Graph
 {
-    matrix = new int[verticies][verticies];
+ GameArena arena = new GameArena(500,500);
+     Line line[] = new Line[4]; 
+    Node nodeArray[] = new Node[4];
+
+
+
+public Graph(){
+    for(int i = 0;i< 4;i++)
+    {
+        if (i==0){
+            nodeArray[i]= new Node(150,250,"BLUE");
+            line[i]= new Line(150,250,150,150,2,"WHITE");
+        }
+        else if(i==1){
+            nodeArray[i] = new Node(250,150,"GREEN");
+            line[i]= new Line(250,150,250,250,2,"WHITE");
+        }
+        else if(i==2){
+            nodeArray[i] = new Node(250,250,"RED");
+            line[i]= new Line(250,250,150,250,2,"WHITE");
+        }
+        else{
+            nodeArray[i]= new Node(150,150,"YELLOW");
+            line[i] = new Line(150,150,250,150,2,"WHITE");
+        }
+    }
+       
+        for( int i=0;i<4;i++)
+        {
+            arena.addBall(nodeArray[i].getNode());
+            arena.addLine(line[i]);
+            
+           
+        }
+
+        
+
+        arena.update();
+        
+    }
 }
 
-public void addEdge(int i , int j)
-{
-    matrix[i][j] = 1;
-    matrix[j][j] = 1;
-}
 
-public void removeEdge(int i , int j)
-{
-    matrix[i][j] = 0; 
-    matrix[j][i]= 0;
-}
 
-public int isEdge(int i , int j)
-{
-    return matrix[i][j];
-}
 
-public boolean pairs(int i, int j )
-{
-    if (matrix[i][j] = 1){
-    return true; }
-}
 
-public static void main(String args[])
-{
-}
-}
+
+
 
