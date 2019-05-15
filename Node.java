a11fec4f16601001a4d2c2fe628da5030a448257
Nizynski <nizynski@egb070000019.lancs.local>
2019-05-15 11:34:55 +0100

    i made my get x and y metods as well as a method to draw the node so that it was above the lines

--- Node.java
+++ Node.java
@@ -1,30 +1,35 @@
 public class Node
 
-
 {
     private Ball b;
+    private GameArena a;
 
     public Node(double x,double y,String colour, GameArena arena)
     {
-        //super(x,y,diameter,colour);
+        a = arena;
         b = new Ball(x,y,20,colour);
-        //a.addBall(b);
+        //arena.addBall(b);
     }   
+
+    public void drawNode()
+    {
+        a.addBall(b);
+    }
 
     public Ball getNode()
     {
-        return b; 
+       return b; 
     }
 
- //   public int getX()
-   // {
-    //    return b.getX(); 
-    //}
+    public double getX()
+    {
+       return b.getXPosition(); 
+    }
 
-    //public int getY()
-    //{
-      //  return b.getY();
-    //}
+   public double getY()
+    {
+      return b.getYPosition();
+    }
 
    
 }
