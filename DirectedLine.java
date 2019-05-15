695132a91b7c249177910ff5e0748be4a9683c27
Nizynski <nizynski@egb070000030.lancs.local>
2019-05-15 20:27:48 +0100

    i have changed the arrow head position so that it is visable on the graph

--- DirectedLine.java
+++ DirectedLine.java
@@ -4,7 +4,7 @@ public class DirectedLine
    public DirectedLine(Node s ,Node e, GameArena arena) 
     {
         a = new Arrow(s.getX(),s.getY(),e.getX(),e.getY(), 6.0,"WHITE",arena);
-        
+        a.setArrowHeadPosition(85.0);
        // arena.addArrow(a);
         arena.update();
     }
