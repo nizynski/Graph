3ad1f1a2f801ec2d03e2e331e07b6c6209f99828
Nizynski <nizynski@egb070000030.lancs.local>
2019-05-15 20:11:19 +0100

    I have moved on to creating the third graph as i have now made a directed line class

--- Graph.java
+++ Graph.java
@@ -13,30 +13,30 @@ public class Graph
 
 
 
-public Graph(){
+public Graph()
+    {
 
-            nodeArray[0]= new Node(150.0,250.0,"BLUE",arena );
-            nodeArray[1] = new Node(250.0,150.0,"GREEN",arena);
-            nodeArray[2] = new Node(250.0,250.0,"RED",arena);
-            nodeArray[3]= new Node(150.0,150.0,"YELLOW",arena);
+        nodeArray[0]= new Node(150.0,250.0,"BLUE",arena );
+        nodeArray[1] = new Node(250.0,150.0,"GREEN",arena);
+        nodeArray[2] = new Node(250.0,250.0,"RED",arena);
+        nodeArray[3]= new Node(150.0,150.0,"YELLOW",arena);
+
         undirectedLines[0]= new UndirectedLine(nodeArray[0],nodeArray[2],arena);
         undirectedLines[0]= new UndirectedLine(nodeArray[1],nodeArray[2],arena);
         undirectedLines[0]= new UndirectedLine(nodeArray[3],nodeArray[1],arena);
         undirectedLines[0]= new UndirectedLine(nodeArray[0],nodeArray[3],arena);
-
-
-
-      
-        
-     for(int i = 0 ; i < 4 ; i++){
+   
+         for(int i = 0 ; i < 4 ; i++)
+         {
          nodeArray[i].drawNode();
-     }
-       
-      
+         }
 
-        
+        // for(int i = 0; i<4; i++){
+           // System.out.println(nodeArray[i]);
+          //  System.out.println(undirectedLines[i]);
+         //}
 
-        arena.update();
+            arena.update();
         
     }
 }
