67c5056c693281b70f57c666b172b51c9140f607
Nizynski <nizynski@egb070000032.lancs.local>
2019-05-20 14:45:08 +0100

    i called the display method in the loop so that it displayed for all 4 archs

--- Graph.java
+++ Graph.java
@@ -16,25 +16,24 @@ public class Graph
 public Graph()
     {
 
-        nodeArray[0]= new Node(150.0,250.0,"BLUE",arena );
-        nodeArray[1] = new Node(250.0,150.0,"GREEN",arena);
-        nodeArray[2] = new Node(250.0,250.0,"RED",arena);
-        nodeArray[3]= new Node(150.0,150.0,"YELLOW",arena);
+        nodeArray[0]= new Node("a",150.0,250.0,"BLUE",arena );
+        nodeArray[1] = new Node("b",250.0,150.0,"GREEN",arena);
+        nodeArray[2] = new Node("c",250.0,250.0,"RED",arena);
+        nodeArray[3]= new Node("d",150.0,150.0,"YELLOW",arena);
 
         undirectedLines[0]= new UndirectedLine(nodeArray[0],nodeArray[2],arena);
-        undirectedLines[0]= new UndirectedLine(nodeArray[1],nodeArray[2],arena);
-        undirectedLines[0]= new UndirectedLine(nodeArray[3],nodeArray[1],arena);
-        undirectedLines[0]= new UndirectedLine(nodeArray[0],nodeArray[3],arena);
+        undirectedLines[1]= new UndirectedLine(nodeArray[1],nodeArray[2],arena);
+        undirectedLines[2]= new UndirectedLine(nodeArray[3],nodeArray[1],arena);
+        undirectedLines[3]= new UndirectedLine(nodeArray[0],nodeArray[3],arena);
+       
    
          for(int i = 0 ; i < 4 ; i++)
          {
-         nodeArray[i].drawNode();
+         nodeArray[i].drawNode(); 
+         undirectedLines[i].displayUndirected();
          }
 
-        // for(int i = 0; i<4; i++){
-           // System.out.println(nodeArray[i]);
-          //  System.out.println(undirectedLines[i]);
-         //}
+       
 
             arena.update();
         
