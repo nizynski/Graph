87f6ba58311f8b0e1c3e46453f35987617829c57
Nizynski <nizynski@egb070000023.lancs.local>
2019-05-20 17:19:43 +0100

    added comments

--- Graph.java
+++ Graph.java
@@ -1,21 +1,27 @@
 //File:Graph.java
 
-/*
+/** 
+* This class calls nodes and lines to make graph 1
 *
 * @author katie nizynski
 */
 
 public class Graph
 {
-    GameArena arena = new GameArena(500,500);
+    GameArena arena = new GameArena(500,500);// creates a game arean 500 by 500
     UndirectedLine undirectedLines[] = new UndirectedLine[4]; 
     Node nodeArray[] = new Node[4];
 
-
+/**Graph3 Method.
+     * 
+     * This method is used to call all the nodes and lines to be created to make the graph
+     * @param 
+     * @return 
+     */
 
 public Graph()
     {
-
+        /** fills the array with nodes and properties */
         nodeArray[0]= new Node("a",150.0,250.0,"BLUE",arena );
         nodeArray[1] = new Node("b",250.0,150.0,"GREEN",arena);
         nodeArray[2] = new Node("c",250.0,250.0,"RED",arena);
@@ -29,8 +35,8 @@ public Graph()
    
          for(int i = 0 ; i < 4 ; i++)
          {
-         nodeArray[i].drawNode(); 
-         undirectedLines[i].displayUndirected();
+         nodeArray[i].drawNode(); // draws the node 
+         undirectedLines[i].displayUndirected();// displays all the connections 
          }
 
        
