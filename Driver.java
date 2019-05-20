d9131fd72b2f5b2d0f99ab2856809d61c6ace2f4
Nizynski <nizynski@egb070000032.lancs.local>
2019-05-20 14:43:16 +0100

    i added ways to call graph 2 and 3 in the driver

--- Driver.java
+++ Driver.java
@@ -1,9 +1,29 @@
+//File:Driver.java
+
+/*
+*
+* @author katie nizynski
+*/
+
 public class Driver{
 
 
 public static void main(String[]args)
+//{
+
+ //Graph graph = new Graph();
+    
+//}
+
+//{
+
+ //Graph2 graph = new Graph2();
+    
+//}
+
 {
 
-    Graph graph = new Graph();
+   Graph3 graph = new Graph3();
+    
 }
 }
