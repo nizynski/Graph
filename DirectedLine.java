42134f1189ff1cf1411faba31b7b3c70f653fce1
Nizynski <nizynski@egb070000032.lancs.local>
2019-05-20 14:41:02 +0100

    I added a method which means each line is named and so that i can print out what nodes are connected

--- DirectedLine.java
+++ DirectedLine.java
@@ -1,12 +1,32 @@
+//File:DirectedLine.java
+
+/*
+*
+* @author katie nizynski
+*/
 public class DirectedLine
 {
    private Arrow a;
+   private Node start;
+   private Node end; 
    public DirectedLine(Node s ,Node e, GameArena arena) 
     {
-        a = new Arrow(s.getX(),s.getY(),e.getX(),e.getY(), 6.0,"WHITE",arena);
+       
+       start = s;
+       end = e;
+         a = new Arrow(s.getX(),s.getY(),e.getX(),e.getY(), 6.0,"WHITE",arena);
         a.setArrowHeadPosition(85.0);
        // arena.addArrow(a);
         arena.update();
     }
+
+
+   public void displayDirected()
+    {
+      System.out.println("Node " + start.getName() + " is connected to " + end.getName());
+    }
+
 }
+
+ 
     
