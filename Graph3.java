fdb6719797f2f92ebb7575fc3740a1c98ef4a386
Nizynski <nizynski@egb070000032.lancs.local>
2019-05-20 15:09:46 +0100

    i have added the display method so that i can show all the connections

--- Graph3.java
+++ Graph3.java
@@ -1,3 +1,11 @@
+//File:Graph3.java
+
+/*
+*
+* @author katie nizynski
+*/
+
+
 public class Graph3
 {
     GameArena arena = new GameArena(500,500);
@@ -9,19 +17,21 @@ public class Graph3
 public Graph3()
     {
 
-        nodeArray[0]= new Node(150.0,250.0,"BLUE",arena );
-        nodeArray[1] = new Node(300.0,250.0,"GREEN",arena);
-        nodeArray[2] = new Node(225.0,400.0,"RED",arena);
-        nodeArray[3]= new Node(225.0,100.0,"YELLOW",arena);
+        nodeArray[0]= new Node("A",150.0,250.0,"BLUE",arena );
+        nodeArray[1] = new Node("B",300.0,250.0,"GREEN",arena);
+        nodeArray[2] = new Node("C",225.0,400.0,"RED",arena);
+        nodeArray[3]= new Node("D",225.0,100.0,"YELLOW",arena);
 
         directedLines[0]= new DirectedLine(nodeArray[0],nodeArray[2],arena);
-        directedLines[0]= new DirectedLine(nodeArray[2],nodeArray[1],arena);
-        directedLines[0]= new DirectedLine(nodeArray[1],nodeArray[3],arena);
-        directedLines[0]= new DirectedLine(nodeArray[3],nodeArray[0],arena);
+        directedLines[1]= new DirectedLine(nodeArray[2],nodeArray[1],arena);
+        directedLines[2]= new DirectedLine(nodeArray[1],nodeArray[3],arena);
+        directedLines[3]= new DirectedLine(nodeArray[3],nodeArray[0],arena);
+       
    
          for(int i = 0 ; i < 4 ; i++)
          {
-         nodeArray[i].drawNode();
+         nodeArray[i].drawNode(); 
+         directedLines[i].displayDirected();
          }
 
     
