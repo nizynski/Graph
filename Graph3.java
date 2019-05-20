ebaa2ff2edaccc1a768f83767db3ad53bfb134b0
Nizynski <nizynski@egb070000023.lancs.local>
2019-05-20 17:18:14 +0100

    added comments

--- Graph3.java
+++ Graph3.java
@@ -1,6 +1,7 @@
 //File:Graph3.java
 
-/*
+/** 
+* This class calls the nodes and lines to create graph 3 
 *
 * @author katie nizynski
 */
@@ -8,15 +9,20 @@
 
 public class Graph3
 {
-    GameArena arena = new GameArena(500,500);
-    DirectedLine directedLines[] = new DirectedLine[4]; 
+    GameArena arena = new GameArena(500,500); // creates a game arena which is 500 by 500
+    DirectedLine directedLines[] = new DirectedLine[4]; // creates an array which can hold 5 elements
     Node nodeArray[] = new Node[4];
 
-
+ /**Graph3 Method.
+     * 
+     * This method is used to call all the nodes and lines to be created to make the graph
+     * @param 
+     * @return 
+     */
 
 public Graph3()
     {
-
+        /** fills the array with nodes and properties */
         nodeArray[0]= new Node("A",150.0,250.0,"BLUE",arena );
         nodeArray[1] = new Node("B",300.0,250.0,"GREEN",arena);
         nodeArray[2] = new Node("C",225.0,400.0,"RED",arena);
@@ -30,8 +36,8 @@ public Graph3()
    
          for(int i = 0 ; i < 4 ; i++)
          {
-         nodeArray[i].drawNode(); 
-         directedLines[i].displayDirected();
+         nodeArray[i].drawNode(); // adds the node 
+         directedLines[i].displayDirected();// displays the connections
          }
 
     
