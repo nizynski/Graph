e05ddc14f4a41727066be4bbd60527bf50d29948
Nizynski <nizynski@egb070000032.lancs.local>
2019-05-20 14:41:48 +0100

    I added a method to this line so that it can name connections

--- UndirectedLine.java
+++ UndirectedLine.java
@@ -1,10 +1,30 @@
+//File:UndirectedLine.java
+
+/** 
+* This class creates Lines without arrows to represent undriected archs connecting the nodes, which can be called in any of the graph classes.
+*
+* @author katie nizynski
+*/
+
+
 public class UndirectedLine
 {
    private Line a;
+   private Node start;
+   private Node end;
    public UndirectedLine(Node s ,Node e, GameArena arena) 
     {
-        a = new Line(s.getX(),s.getY(),e.getX(),e.getY(), 2, "WHITE");
+        start = s;
+        end = e;
+        a = new Line(s.getX(),s.getY(),e.getX(),e.getY(),2,"WHITE");
         arena.addLine(a);
+
+        arena.update();
     }
-    
+
+    public void displayUndirected()
+    {
+      System.out.println("Node " + start.getName() + "is connected to " + end.getName());
+    }
+ 
 }
